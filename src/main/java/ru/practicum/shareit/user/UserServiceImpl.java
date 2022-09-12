5ca71ce53f0d254d0eaec1600b55a3860c43d07d
lilyerma <lilyerm@gmail.com>
2022-09-12 18:51:40 +0300

    Improved on comments

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -45,7 +45,9 @@ public class UserServiceImpl implements UserService {
     public UserDto update(UserDto userDto, long id) {
         checkId(id);
         User existUser = userRepository.getReferenceById(id);
-        existUser.setEmail(userDto.getEmail());
+        if (userDto.getEmail()!=null){
+            existUser.setEmail(userDto.getEmail());
+        }
         if (userDto.getName()!=null){
             existUser.setName(userDto.getName());
         }
